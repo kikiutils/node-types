4d2ca6238004f74d301443808adb98ae088e10bb
kiki-kanri <a470666@gmail.com>
2024-07-22 15:28:54 +0800

    chore: move utils types to root directory and rename to index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,42 @@
+export declare global {
+	/**
+	 * A dictionary type that maps string keys to values of type T.
+	 *
+	 * @template T - The type of the values in the dictionary.
+	 */
+	type Dict<T> = Record<string, T>;
+
+	/**
+	 * A type that represents either a value of type T or null.
+	 *
+	 * @template T - The type of the value.
+	 */
+	type Nullable<T> = T | null;
+
+	/**
+	 * A type that represents either a number or null.
+	 */
+	type NullableNumber = Nullable<number>;
+
+	/**
+	 * A type that represents either a string or null.
+	 */
+	type NullableString = Nullable<string>;
+
+	/**
+	 * A type that represents either a value of type T or undefined.
+	 *
+	 * @template T - The type of the value.
+	 */
+	type Undefinedable<T> = T | undefined;
+
+	/**
+	 * A type that represents either a number or undefined.
+	 */
+	type UndefinedableNumber = Undefinedable<number>;
+
+	/**
+	 * A type that represents either a string or undefined.
+	 */
+	type UndefinedableString = Undefinedable<string>;
+}
--- types/utils.d.ts
+++
@@ -1,9 +0,0 @@
-export declare global {
-	type Dict<T> = Record<string, T>;
-	type Nullable<T> = T | null;
-	type NullableNumber = Nullable<number>;
-	type NullableString = Nullable<string>;
-	type Undefinedable<T> = T | undefined;
-	type UndefinedableNumber = Undefinedable<number>;
-	type UndefinedableString = Undefinedable<string>;
-}
