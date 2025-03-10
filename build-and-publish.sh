f780574a6a9eca9fc6a8e2215de292aafb4d8cc3
kiki-kanri <a470666@gmail.com>
2025-03-10 13:06:31 +0800

    style: format script

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -e
 cd "$(dirname "$(readlink -f "$0")")"
-pnpm run build && cp ./LICENSE ./package.json ./README.md ./dist && cd ./dist && npm publish
+pnpm run build
+cp ./LICENSE \
+    ./package.json \
+    ./README.md \
+    ./dist
+
+cd ./dist
+npm publish
