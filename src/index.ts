e6ebc6f7c26d95198b16a15c68a30e9fe15f9d5c
kiki-kanri <a470666@gmail.com>
2025-03-10 13:05:39 +0800

    refactor!: remove deprecated codes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import type { Promisable } from 'type-fest';
-
 export type {} from './utils/filtered-key-path';
-
-export type DoNotRemoveOrUseThisType = Promisable<any>;
 
 declare global {
     /**
@@ -30,13 +26,6 @@ declare global {
     type NullableString = Nullable<string>;
     type PartialDict<T> = Partial<Dict<T>>;
     type PartialRecord<K extends keyof any, T> = Partial<Record<K, T>>;
-
-    /**
-     * @deprecated Use {@link Promisable} from `type-fest` instead.
-     *
-     * Alternatively, import the `type-fest` file from this package and directly use the {@link Promisable} type.
-     */
-    type Promiseable<T> = Promise<T> | T;
     type Readonlyable<T> = Readonly<T> | T;
     type ReadonlyDict<T> = Readonly<Dict<T>>;
     type ReadonlyRecord<K extends keyof any, T> = Readonly<Record<K, T>>;
@@ -57,27 +46,4 @@ declare global {
      * A type that represents either a string or undefined.
      */
     type UndefinedableString = Undefinedable<string>;
-
-    /**
-     * A type that represents either a value of type T or undefined.
-     *
-     * @deprecated Use {@link Undefinedable} instead.
-     *
-     * @template T - The type of the value.
-     */
-    type UndefinedAble<T> = T | undefined;
-
-    /**
-     * A type that represents either a number or undefined.
-     *
-     * @deprecated Use {@link UndefinedableNumber} instead.
-     */
-    type UndefinedAbleNumber = UndefinedAble<number>;
-
-    /**
-     * A type that represents either a string or undefined.
-     *
-     * @deprecated Use {@link UndefinedableString} instead.
-     */
-    type UndefinedAbleString = UndefinedAble<string>;
 }
