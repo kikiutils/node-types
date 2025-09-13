3e3699462bae725949474d75df56cd01fe8df5a5
kiki-kanri <a470666@gmail.com>
2025-09-13 20:59:57 +0800

    refactor!: remove Except due to type-fest v5 update; import separately when needed

--- src/type-fest.ts
+++ src/type-fest.ts
@@ -1,42 +1,16 @@
 import type {
     Arrayable as TArrayable,
-    Except as TExcept,
     Promisable as TPromisable,
     ReadonlyDeep as TReadonlyDeep,
     SetOptional as TSetOptional,
     SetRequired as TSetRequired,
 } from 'type-fest';
-import type { ApplyDefaultOptions } from 'type-fest/source/internal';
-
-interface ExceptOptions {
-    /**
-     * Disallow assigning non-specified properties.
-     *
-     * Note that any omitted properties in the resulting type will be present in autocomplete as `undefined`.
-     *
-     * @default false
-     */
-    requireExactProps?: boolean;
-}
 
 declare global {
     /**
      * @see {@link TArrayable}
      */
     type Arrayable<T> = TArrayable<T>;
-
-    /**
-     * @see {@link TExcept}
-     */
-    type Except<
-        ObjectType,
-        KeysType extends keyof ObjectType,
-        Options extends ExceptOptions = object,
-    > = TExcept<
-        ObjectType,
-        KeysType,
-        ApplyDefaultOptions<ExceptOptions, { requireExactProps: false }, Options>
-    >;
 
     /**
      * @see {@link TPromisable}
